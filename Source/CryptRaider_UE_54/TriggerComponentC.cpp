7f8bd13c1d278fe37f2a8b6635fa8c33fc659c81
Chiraag Rokade <94799807+ChiraagRokade@users.noreply.github.com>
2025-12-11 17:14:20 +0530

    while and for loop

--- Source/CryptRaider_UE_54/TriggerComponentC.cpp
+++ Source/CryptRaider_UE_54/TriggerComponentC.cpp
@@ -3,11 +3,40 @@
 
 #include "TriggerComponentC.h"
 
+UTriggerComponentC::UTriggerComponentC()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
 void UTriggerComponentC::BeginPlay()
 {
 	Super::BeginPlay();
 
 	// ...
-	// UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
-    UE_LOG(LogTemp, Display, TEXT("test"));
+    // UE_LOG(LogTemp, Display, TEXT("test"));
+}
+
+void UTriggerComponentC::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction){
+    
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    TArray<AActor*> Actors;
+    GetOverlappingActors(Actors);
+
+    // if(Actors.Num() > 0){
+    //     FString ActorName = Actors[0]->GetActorNameOrLabel();
+    //     UE_LOG(LogTemp, Display, TEXT("Overlapping Actor: %s"), *ActorName);
+    // }
+
+    // int32 i = 0;
+    // while(i < Actors.Num()){
+    //     FString ActorName = Actors[i]->GetActorNameOrLabel();
+    //     UE_LOG(LogTemp, Display, TEXT("Overlapping Actor %d: %s"), i, *ActorName);
+    //     ++i;
+    // }
+
+    for(int32 index = 0; index<Actors.Num(); index++){
+        FString ActorName = Actors[index]->GetActorNameOrLabel();
+        UE_LOG(LogTemp, Display, TEXT("Overlapping Actor %d: %s"), index, *ActorName);
+    }
 }
--- Source/CryptRaider_UE_54/TriggerComponentC.h
+++ Source/CryptRaider_UE_54/TriggerComponentC.h
@@ -14,11 +14,15 @@ class CRYPTRAIDER_UE_54_API UTriggerComponentC : public UBoxComponent
 {
 	GENERATED_BODY()
 	
+public:	
+	// Sets default values for this component's properties
+	UTriggerComponentC();
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-// public:	
-// 	// Called every frame
-// 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 };
